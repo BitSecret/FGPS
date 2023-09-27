326f07ea1684dd438b8c33eb0c383d7efb971081
xiaokaizhang <xiaokaizhang1999@163.com>
2023-09-27 15:31:43 +0800

    format search result

--- utils/search/clean.py
+++ utils/search/clean.py
@@ -5,16 +5,19 @@ from utils.utils import safe_save_json
 path_search_data = "../../datasets/solved/search/"
 
 
-def clean_error(error_name):
-    """error_name=[KeyError, RuntimeError, MemoryError]"""
+def clean_error(error_msg):
+    """error_name: [KeyError, RuntimeError, MemoryError, TypeError, IndexError, KeyboardInterrupt]"""
     for d in direction:
         for m in method:
             log = load_json("{}-{}.json".format(d, m))
             data = load_json(path_search_data + "{}-{}.json".format(d, m))
+            pop_pids = []
             for pid in data["error"]:
-                if data["error"][pid]["msg"].startswith(error_name):
-                    data["error"].pop(pid)
-                    log["error_pid"].pop(int(pid))
+                if data["error"][pid]["msg"].startswith(error_msg):
+                    pop_pids.append(pid)
+            for pid in pop_pids:
+                data["error"].pop(pid)
+                log["error_pid"].pop(log["error_pid"].index(int(pid)))
 
             safe_save_json(log, "", "{}-{}".format(d, m))
             safe_save_json(data, path_search_data, "{}-{}".format(d, m))
@@ -26,10 +29,14 @@ def clean_timeout(timeout):
         for m in method:
             log = load_json("{}-{}.json".format(d, m))
             data = load_json(path_search_data + "{}-{}.json".format(d, m))
+            pop_pids = []
             for pid in data["timeout"]:
                 if data["timeout"][pid]["msg"] == timeout:
-                    data["timeout"].pop(pid)
-                    log["timeout_pid"].pop(int(pid))
+                    pop_pids.append(pid)
+
+            for pid in pop_pids:
+                data["timeout"].pop(pid)
+                log["timeout_pid"].pop(log["error_pid"].index(int(pid)))
 
             safe_save_json(log, "", "{}-{}".format(d, m))
             safe_save_json(data, path_search_data, "{}-{}".format(d, m))
@@ -46,5 +53,29 @@ def clean_unsolved():
             safe_save_json(data, path_search_data, "{}-{}".format(d, m))
 
 
+def show_unhandled():
+    print("direction\tmethod\tunhandled")
+    for d in direction:
+        for m in method:
+            unhandled = []
+            log = load_json("{}-{}.json".format(d, m))
+            for pid in range(1, 6982):
+                if pid in log["solved_pid"]:
+                    continue
+                if pid in log["unsolved_pid"]:
+                    continue
+                if pid in log["error_pid"]:
+                    continue
+                if pid in log["timeout_pid"]:
+                    continue
+                unhandled.append(pid)
+
+            print("{}\t{}\t{}".format(d, m, unhandled))
+
+
 if __name__ == '__main__':
-    clean_error("KeyError")
+    # clean_error(error_msg="RuntimeError(\"can't start new thread\")")
+    clean_error(error_msg="TypeError")
+    # clean_timeout(timeout=200)
+    # clean_unsolved()
+    show_unhandled()
